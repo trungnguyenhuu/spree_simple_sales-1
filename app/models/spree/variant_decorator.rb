0bc2471824f625a243c0fd5e898fc76274bc34bb
Herman Moreno <herman.moreno@crowdint.com>
2014-02-13 15:54:55 -0600

    Validate sale price below normal price

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,6 +2,6 @@ Spree::Variant.class_eval do
   attr_accessible :sale_price
 
   def on_sale?
-    sale_price.present? && sale_price > 0
+    sale_price.present? && (sale_price > 0) && (price > sale_price)
   end
 end
